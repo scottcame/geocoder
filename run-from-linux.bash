6dfa55803812caebcaf944dacafb0630caf68014
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-04-19 10:58:26 -0700

    fetch the right files

--- run-from-linux.bash
+++ run-from-linux.bash
@@ -12,9 +12,9 @@ echo ""
 docker exec -u postgres -w /usr/local/src/geocoder_scripts geocoder_geocoder_1 \
   /usr/local/src/geocoder_scripts/create-geocoder-database.bash
 echo "Retriving database backup"
-docker cp geocoder_geocoder_1:/gisdata/geocoder.backup ./Raw
+docker cp geocoder_geocoder_1:/gisdata/geocoder.sql.gz ./Raw
 echo "Retriving shapefile archive"
 docker cp geocoder_geocoder_1:/gisdata/tiger.zip ./Raw
 echo "Retriving database population scripts"
 docker cp geocoder_geocoder_1:/gisdata/nation.bash ./Raw
-docker cp geocoder_geocoder_1:/gisdata/oregon.bash ./Raw
+docker cp geocoder_geocoder_1:/gisdata/states.bash ./Raw
